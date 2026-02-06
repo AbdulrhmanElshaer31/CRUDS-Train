3a5aa6c07fad31ee6ce33348b6b13e1caddc4c0a
AbdulrhmanElshaer31 <abdelrhmanelshaer31gmail.com>
2026-02-06 17:12:35 +0200

    project init

---
+++ src/actions/products.js
@@ -0,0 +1,17 @@
+export async function product() {
+    try {
+        const request = await fetch('https://fakestoreapi.com/products', {
+            method:"GET"
+        })
+        const response = await request.json();
+        if (!request.ok) {
+            return null;
+        }
+        console.log(response);
+        
+        return response;
+    } catch (err) {
+        console.error(err);
+        
+    }
+}
