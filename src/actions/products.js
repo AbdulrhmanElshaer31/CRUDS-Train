e9b111367a97d98f2874a58ed8aeda0e0f590f2b
AbdulrhmanElshaer31 <abdelrhmanelshaer31gmail.com>
2026-02-09 00:08:18 +0200

    folders added !

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,19 +1,173 @@
-export async function product() {
+
+// This file contains CRUDS opreation for products
+const apiUrl = 'https://fakestoreapi.com';
+
+//Get all products
+
+export async function getAllProducts() {
     try {
-        const request = await fetch('https://fakestoreapi.com/products', {
-            method:"GET"
-        })
+        const request = await fetch(`${apiUrl}/products`, {
+            method: "GET",
+            headers: {
+                "content-type": "application/json"
+            }
+        });
         const response = await request.json();
         if (!request.ok) {
-            return null;
+            return {
+                message: "Can't laod products please refresh the page!",
+                success: false,
+                data : null
+            }
         }
-        console.log(response);
-        
-        return response;
-    } catch (err) {
-        console.error(err);
-        
+            return {
+                message: "Products loaded successfully!",
+                success: true,
+                data:response
+            }
+    } catch(err) {
+            console.error("Error: ",err);
+        return {
+                message: "internet connection error",
+                success: false,
+                data : null
+            }
     }
 }
 
+//Get product by id
+
+export async function getProductById(id) {
+    try {
+        const request = await fetch(`${apiUrl}/products/${id}`, {
+            method: "GET",
+            headers: {
+                "content-type": "application/json"
+            }
+        });
+        const response = await request.json();
+        if (!request.ok) {
+            return {
+                message: "Can't laod product data please refresh the page!",
+                success: false,
+                data : null
+            }
+        }
+            return {
+                message: "Product data loaded successfully!",
+                success: true,
+                data:response
+            }
+    } catch(err) {
+            console.error("Error: ",err);
+        return {
+                message: "internet connection error",
+                success: false,
+                data : null
+            }
+    }
+}
+
+//Create product
+
+export async function creatProduct( title, price, description, category, image ) {
+    try {
+        const request = await fetch(`${apiUrl}/products`, {
+            method: "POST",
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify({
+                title,
+                price,
+                description,
+                category,
+                image
+            }) 
+        })
+
+        if (!request.ok) {
+            return {
+                message: "Failed to add product please try Again later",
+                success: false
+            }
+        }
+        return {
+            message: "Product added successfully!",
+            success: true
+        }
+    } catch (error) {
+        console.error("error: ", error);
+                return {
+                message: "Please check internet connection!",
+                success: false
+            }
+        
+    }
+    
+}
+
+//Edit product
+
+export async function editProduct( title, price, description, category, image,id) {
+    try {
+        const request = await fetch(`${apiUrl}/products/${id}`, {
+            method: "POST",
+            headers: {
+                "content-type" : "application/json"
+            },
+            body: JSON.stringify({
+                title,
+                price,
+                description,
+                category,
+                image
+            }) 
+        })
+        if (!request.ok) {
+            return {
+                message: "Failed to edit product please try Again later",
+                success: false
+            }
+        }
+        return {
+            message: "Product edited successfully!",
+            success: true
+        }
+    } catch (error) {
+        console.error("error: ", error);
+                return {
+                message: "Please check internet connection!",
+                success: false
+            }
+        
+    }
+    
+}
+
+//Delete product
+
+export async function deleteProduct(id) {
+    try {
+        const request = await fetch(`${apiUrl}/products/${id}`, {method: "DELETE"})
+        if (!request.ok) {
+            return {
+                message: "Failed to delete product please try Again later",
+                success: false
+            }
+        }
+        return {
+            message: "Product deleted successfully!",
+            success: true
+        }
+    } catch (error) {
+        console.error("error: ", error);
+                return {
+                message: "Please check internet connection!",
+                success: false
+            }
+        
+    }
+    
+}
 
